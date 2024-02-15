9df6b41066fbbd1c428b386ebf4e60512757e73f
Michael Birchmeier <mbirchmeier@gmail.com>
2024-02-15 15:58:10 -0800

    Initial display of board

---
+++ CalendarPuzzle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace calendar
+{
+    public class CalendarPuzzle
+    {
+        char[][] board = new char[8][];
+
+        public CalendarPuzzle() 
+        {
+            for (int x = 0; x != 8; x++)
+            {
+                board[x] = new char[7];
+                for (int y = 0; y != 7; y++)
+                {
+                    board[x][y] = '_';
+                }
+            }
+            board[0][6] = '*';
+            board[1][6] = '*';
+            board[7][0] = '*';
+            board[7][1] = '*';
+            board[7][2] = '*';
+            board[7][3] = '*';
+        }
+
+        public void DisplayBoard() {
+            for (int x = 0; x != 8; x++)
+            {
+                Console.WriteLine(new string(board[x]));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+var x = new calendar.CalendarPuzzle();
+x.DisplayBoard();
